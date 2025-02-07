139f149c86c5bc298dc6aeaa491c01d41993d4d4
Hércules de Sousa Silva <herculesdeso@gmail.com>
2025-02-07 10:07:52 -0300

    Cria formulário para adicionar médico

--- public/src/app/doctors/page.tsx
+++ public/src/app/doctors/page.tsx
@@ -15,9 +15,20 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const DoctorsPage = async () => {
   const axiosHttpClient = new AxiosHttpClientImpl("http://localhost:8080");
@@ -42,6 +53,60 @@ const DoctorsPage = async () => {
         </header>
 
         <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="flex flex-row justify-end">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button>Add Doctor</Button>
+              </SheetTrigger>
+
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Add doctor</SheetTitle>
+                  <SheetDescription>
+                    Add a new doctor here. Click save when you're done.
+                  </SheetDescription>
+                </SheetHeader>
+
+                <div className="flex flex-col gap-6 pt-6">
+                  <div className="flex flex-col gap-3 items-start">
+                    <Label htmlFor="name">Name</Label>
+
+                    <Input id="name" />
+                  </div>
+
+                  <div className="flex flex-col gap-3 items-start">
+                    <Label htmlFor="email">E-mail</Label>
+
+                    <Input id="email" />
+                  </div>
+
+                  <div className="flex flex-col gap-3 items-start">
+                    <Label htmlFor="phone-number">Phone Number</Label>
+
+                    <Input id="phone-number" />
+                  </div>
+
+                  <div className="flex flex-col gap-3 items-start">
+                    <Label htmlFor="specialty">Specialty</Label>
+
+                    <Input id="specialty" />
+                  </div>
+
+                  <div className="flex flex-col gap-3 items-start">
+                    <Label htmlFor="crm-number">CRM Number</Label>
+
+                    <Input id="crm-number" />
+                  </div>
+                </div>
+
+                <SheetFooter className="mt-4">
+                  <SheetClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
+          </div>
           <DataTable columns={columns} data={doctors} />
         </div>
       </SidebarInset>
